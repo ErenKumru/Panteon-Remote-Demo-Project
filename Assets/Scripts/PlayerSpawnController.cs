ee0f65ef02812fe414b8847803fcf6c34cf7c3b1
Eren Kumru <eren_kumru@hotmail.com>
2021-08-22 00:37:05 +0300

    Add Extendable Spawn Controller and Spawn Mechanics
    Various Respawn mechanics for various character types can easily be added

---
+++ Assets/Scripts/PlayerSpawnController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnController : SpawnController
+{
+    [SerializeField] private float transportSpeed;
+    private Vector3 spawnPoint;
+
+    private void Start()
+    {
+        spawnPoint = transform.position;
+    }
+
+    public override void RespawnCharacter()
+    {
+        StartCoroutine(RespawnAtStartPoint());
+    }
+
+    //DO ANIMATION
+    private IEnumerator RespawnAtStartPoint()
+    {
+        Debug.Log(gameObject.name + " called RespawnAtStartPoint");
+        PlayerController playerController = GetComponent<PlayerController>();
+        playerController.enabled = false;
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+        Collider playerCollider = GetComponent<Collider>();
+        playerCollider.enabled = false;
+
+        while (transform.position.z != spawnPoint.z)
+        {
+            Debug.Log("transporting " + gameObject.name);
+            transform.position = Vector3.MoveTowards(transform.position, spawnPoint, transportSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+
+        playerCollider.enabled = true;
+        rigidbody.useGravity = true;
+        playerController.enabled = true;
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpawnController : MonoBehaviour
+{
+    public abstract void RespawnCharacter();
+}
