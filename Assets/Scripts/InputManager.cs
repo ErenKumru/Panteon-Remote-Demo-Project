0ffffdc3d3e3afe48c1f576a2d6e9eabb549f174
Eren Kumru <eren_kumru@hotmail.com>
2021-08-22 00:27:02 +0300

    Add Swerve mechanic
    Detailed InputManager and PlayerController

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float thresholdDistance;
+
+    private Vector2 initialInputPosition;
+    private Vector2 currentInputPosition;
+    private Vector2 effectiveInputVector;
+
+    private void Update()
+    {
+        SwerveCircleInput();
+    }
+
+    private void SwerveCircleInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            initialInputPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            currentInputPosition = Input.mousePosition;
+
+            Vector2 inputDifference = currentInputPosition - initialInputPosition;
+            effectiveInputVector = inputDifference.magnitude > thresholdDistance ? inputDifference.normalized : Vector2.zero;
+        }
+        else effectiveInputVector = Vector2.zero;
+    }
+
+    public Vector2 GetEffectiveInputVector()
+    {
+        return effectiveInputVector;
+    }
+
+    public bool HasEffectiveInput()
+    {
+        if (effectiveInputVector != Vector2.zero) return true;
+        return false;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float rotationTime = 0.1f;
+
+    private Animator animator; //trigger animations
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        MoveCharacter();
+        RotateCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 newPosition;
+        newPosition = transform.position;
+        newPosition.x += runSpeed * Time.deltaTime * inputManager.GetEffectiveInputVector().x;
+        newPosition.z += runSpeed * Time.deltaTime * inputManager.GetEffectiveInputVector().y;
+        transform.position = newPosition;
+    }
+
+    private void RotateCharacter()
+    {
+        if(inputManager.HasEffectiveInput())
+        {
+            Quaternion lookDirection = transform.rotation;
+            lookDirection.x = inputManager.GetEffectiveInputVector().x;
+            lookDirection.z = inputManager.GetEffectiveInputVector().y;
+            float targetAngle = Mathf.Atan2(lookDirection.x, lookDirection.z) * Mathf.Rad2Deg;
+            float rotationAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, rotationTime);
+            transform.rotation = Quaternion.Euler(0f, rotationAngle, 0f);
+        }
     }
 }
