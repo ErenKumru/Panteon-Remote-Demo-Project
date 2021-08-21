39cacc94a46f7ea7a16740938af3df899b2c48fd
Eren Kumru <eren_kumru@hotmail.com>
2021-08-22 00:31:16 +0300

    Add Opponents
    AI opponents movement done
    Bumping behaviour is not done!

---
+++ Assets/Scripts/OpponentController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OpponentController : MonoBehaviour
+{
+    [SerializeField] private Transform finishLine;
+    [SerializeField] private float avoidancePredictionTime;
+
+    private NavMeshAgent navMeshAgent;
+    private float finishLineZ;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        finishLineZ = finishLine.position.z;
+        NavMesh.avoidancePredictionTime = avoidancePredictionTime;
+
+        //OpponentMovement();
+    }
+
+    private void Update()
+    {
+        OpponentMovement();
+    }
+
+    //trigger animations
+    private void OpponentMovement()
+    {
+        Vector3 destination;
+        destination = transform.position;
+        destination.z = finishLineZ;
+        navMeshAgent.destination = destination;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(name + " triggered with " + other.gameObject.name);
+        //DO STOP BOTH THIS AND OTHER GAMEOBJECT FOR A LITTLE
+        //ALSO DO ANIMATION
+    }
+}
