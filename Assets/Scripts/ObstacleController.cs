90e4db0499499421b390a6096a2cda1bb92dca66
Eren Kumru <eren_kumru@hotmail.com>
2021-08-22 00:37:37 +0300

    Add Spawn Mechanic
    Mandatory spawn mechanic in game is done

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(name + " collided with " + other.gameObject.name);
+        SpawnController spawnController = other.gameObject.GetComponent<SpawnController>();
+        spawnController.RespawnCharacter();
+    }
+}
