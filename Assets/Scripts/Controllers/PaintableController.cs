f118ffc44ff0187c0e7f96e0231b4b5a5510995f
Eren Kumru <eren_kumru@hotmail.com>
2021-08-26 12:43:03 +0300

    Add painting the wall

---
+++ Assets/Scripts/Controllers/PaintableController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintableController : MonoBehaviour
+{
+    [SerializeField] private float width;
+    [SerializeField] private float height;
+    [SerializeField] private float checkDistance;
+    private float surfaceArea; //The difference between RealArea and manually CalculatedArea is 314,5172 - 314,496 = 0,0212 which can or cannot be ignored
+    private float paintedPercentage;
+
+    private List<Vector3> paintedPositions = new List<Vector3>();
+
+    private void Awake()
+    {
+        CalculatePaintableArea();
+    }
+
+    //Calculates area defined by the world coordinates of the mesh. Width and height values are measured through raycast. CalculatedArea = 314,496
+    private void CalculatePaintableArea()
+    {
+        surfaceArea = width * height;
+        surfaceArea /= 0.1f;
+    }
+
+    public void AddPaintedPosition(Vector3 position)
+    {
+        if (paintedPositions != null && Mathf.Abs(position.z - checkDistance) < 0.1f)
+        {
+            if (paintedPositions.Count == 0) paintedPositions.Add(position);
+            else
+            {
+                bool canAdd = true;
+
+                foreach (Vector3 paintedPosition in paintedPositions)
+                {
+                    if (Vector3.Distance(position, paintedPosition) < 0.1f)
+                    {
+                        canAdd = false;
+                    }
+                }
+
+                if (canAdd) paintedPositions.Add(position);
+            }
+        }
+    }
+
+    //public List<Vector3> GetPaintedPositions()
+    //{
+    //    return paintedPositions;
+    //}
+
+    private float CalculatePaintedPercentage()
+    {
+        return (paintedPositions.Count / surfaceArea * 100f);
+    }
+
+    public float GetPaintedPercentage()
+    {
+        paintedPercentage = CalculatePaintedPercentage();
+        return paintedPercentage;
+    }
+
+    //Calculates real surface area of the surface that is looking towards to camera. RealArea = 314,5172
+    //private float CalculateFacingArea(Mesh mesh, Vector3 direction)
+    //{
+    //    direction = direction.normalized;
+    //    var triangles = mesh.triangles;
+    //    var vertices = mesh.vertices;
+
+    //    double sum = 0.0;
+
+    //    for (int i = 0; i < triangles.Length; i += 3)
+    //    {
+    //        Vector3 corner = vertices[triangles[i]];
+    //        Vector3 a = vertices[triangles[i + 1]] - corner;
+    //        Vector3 b = vertices[triangles[i + 2]] - corner;
+
+    //        float projection = Vector3.Dot(Vector3.Cross(b, a), direction);
+    //        if (projection > 0f)
+    //            sum += projection;
+    //    }
+
+    //    return (float)(sum / 2.0);
+    //}
+}
---
+++ Assets/Scripts/Controllers/PainterController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PainterController : MonoBehaviour
+{
+    [SerializeField] private Camera painterCamera;
+    [SerializeField] private GameObject brush;
+    [SerializeField] private GameObject paintableObject;
+
+    private LineRenderer lineRenderer;
+    private Vector3 lastPosition;
+    private PaintableController paintableController;
+
+    private void Awake()
+    {
+        paintableController = FindObjectOfType<PaintableController>();
+    }
+
+    private void Update()
+    {
+        Paint();
+    }
+
+    private void Paint()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            CreateBrush();
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = painterCamera.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(painterCamera.transform.position, ray.direction * 30f, Color.green);
+
+            //check if ray hit
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit))
+            {
+                //check if ray is at the same position
+                Vector3 currentPos = rayHit.point;
+                if (lineRenderer != null && currentPos != lastPosition && Vector3.Distance(currentPos, lastPosition) > 0.1f /*&& CandAddPoint(rayHit.point)*/)
+                {
+                    AddPoint(currentPos);
+                    lastPosition = currentPos;
+                }
+            }
+        }
+        else
+        {
+            lineRenderer = null;
+        }
+    }
+
+    private void CreateBrush()
+    {
+        GameObject brushInstance = Instantiate(brush);
+        lineRenderer = brushInstance.GetComponent<LineRenderer>();
+
+        Ray ray = painterCamera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(painterCamera.transform.position, ray.direction, Color.green, 100f);
+
+        //check if ray hit
+        RaycastHit rayHit;
+        if (Physics.Raycast(ray, out rayHit) /*&& CandAddPoint(rayHit.point)*/)
+        {
+            lineRenderer.SetPosition(0, rayHit.point);
+            lineRenderer.SetPosition(1, rayHit.point);
+        }
+    }
+
+    private void AddPoint(Vector3 pointPosition)
+    {
+        if(lineRenderer.positionCount == 0)
+        {
+            lineRenderer.SetPosition(0, pointPosition);
+            lineRenderer.SetPosition(1, pointPosition);
+
+        }
+        else
+        {
+            lineRenderer.positionCount++;
+            int positionIndex = lineRenderer.positionCount - 1;
+            lineRenderer.SetPosition(positionIndex, pointPosition);
+            paintableController.AddPaintedPosition(pointPosition);
+        }
+    }
+
+
+    //Should work but line renderer draws lines from (0,0,0) to next position when the starting point is in range of painted position. Don't know why.
+    //private bool CandAddPoint(Vector3 currentPosition)
+    //{
+    //    if (paintableController != null)
+    //    {
+    //        List<Vector3> paintedPositions = paintableController.GetPaintedPositions();
+
+    //        if (paintedPositions != null)
+    //        {
+    //            if (paintedPositions.Count == 0) return true;
+
+    //            foreach (Vector3 paintedPosition in paintedPositions)
+    //            {
+    //                if (Vector3.Distance(currentPosition, paintedPosition) < 0.1f)
+    //                {
+    //                    return false;
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //    return true;
+    //}
+}
