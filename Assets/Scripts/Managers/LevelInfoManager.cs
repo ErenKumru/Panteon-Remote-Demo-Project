93850f83be16edf85986a50e76b5563aef6742fd
Eren Kumru <eren_kumru@hotmail.com>
2021-08-26 12:38:13 +0300

    Add dynamic ranking system

---
+++ Assets/Scripts/Managers/LevelInfoManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfoManager : MonoBehaviour
+{
+    private List<Rankable> characterRankings = new List<Rankable>();
+    private Rankable playerCharacter;
+    private int playerRanking;
+
+    private void Awake()
+    {
+        FindAllCharacters();
+    }
+
+    private void FindAllCharacters()
+    {
+        Rankable[] rankables = FindObjectsOfType<Rankable>();
+
+        foreach(Rankable rankable in rankables)
+        {
+            characterRankings.Add(rankable);
+        }
+
+        playerCharacter = FindObjectOfType<PlayerController>().GetComponent<Rankable>();
+    }
+
+    private void SortAllCharacterRankings()
+    {
+        characterRankings.Sort();
+    }
+
+    private void CalculatePlayerRanking()
+    {
+        playerRanking = characterRankings.IndexOf(playerCharacter);
+    }
+
+    public int GetPlayerRanking()
+    {
+        SortAllCharacterRankings();
+        CalculatePlayerRanking();
+        return playerRanking;
+    }
+
+    public int GetTotalNumberOfCharacters()
+    {
+        return characterRankings.Count;
+    }
+}
---
+++ Assets/Scripts/Rankable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Rankable : MonoBehaviour, IComparable<Rankable>
+{
+    public int CompareTo(Rankable other)
+    {
+        return other.transform.position.z.CompareTo(transform.position.z);
+    }
+}
