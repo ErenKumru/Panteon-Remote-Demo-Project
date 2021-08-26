ba6f02a3146c4d1fd252fcb8e69c9c1e4cd84113
Eren Kumru <eren_kumru@hotmail.com>
2021-08-26 12:40:11 +0300

    Add sound manager and music

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource musicPlayer;
+
+    public bool MuteUnmuteSound()
+    {
+        return musicPlayer.mute = !musicPlayer.mute;
+    }
+}
